2e1c987796160ae2db46599950e6124eff2abdbf
Marski <komar_16@o2.pl>
2021-05-30 20:17:09 +0200

    clean code

---
+++ src/model/OfferOptionModel.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Idea\model;
+
+use Idea\model\AbstractModel;
+use Idea\exception\IdeaException;
+use PDO;
+use PDOException;
+
+
+class OfferOptionModel extends AbstractModel
+{
+    public function get()
+    {
+        try {
+            $stmt = $this->DB->query('SELECT * FROM offer_option');
+            $stmt->execute();
+        } catch (PDOException $e) {
+            throw new IdeaException('Błąd Pobierania ustawień');
+        }
+
+        if ($stmt->rowCount() > 0) {
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $param[] = $row;
+            }
+            return $param;
+        } else {
+            echo 'Brak elementów do wyświetlenia';
+        }
+    }
+}
